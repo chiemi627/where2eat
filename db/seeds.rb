82aef1a6272117873c66e114417dc8e8143c50a7
Chiemi Watanabe <chiemi@slis.tsukuba.ac.jp>
2019-05-17 14:29:29 +0900

    お店の基本情報を追加した

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Eatery.delete_all
+Eatery.create(name:"SALON DE CENT", address:"〒305-0005 茨城県つくば市天久保２丁目２０−７ レガートホソダ",tel:"029-858-7886",regular_holiday:"日曜日",open:"12:00",close:"21:30")
+Eatery.create(name:"カレーうどん ZEYO", address:"〒305-0005 茨城県つくば市天久保２丁目６−１ ベスト ランド Ⅳ １Ｆ",tel:"029-854-1778",regular_holiday:"なし",open:"11:30",close:"23:00")
